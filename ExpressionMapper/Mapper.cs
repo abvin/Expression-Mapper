c63514ea4d628a1463060ca564e1f045a3149bd0
Abhilash Vinod <abvin@outlook.com>
2017-02-02 21:11:41 -0600

    Add Mapper class

---
+++ ExpressionMapper/Mapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressionMapper
+{
+    public class ParentClass
+    {
+        [MapperAttribute(Key = "TEST_KEY")]
+        public int MyProperty { get; set; }
+        public string MyStringProperty { get; set; }
+    }
+
+    internal class MapperAttribute : Attribute
+    {
+        public string Key { get; set; }
+    }
+
+    class Mapper<TRequest>
+    {
+        Dictionary<string, Func<TRequest, object>> _mappers = new Dictionary<string, Func<TRequest, object>>();
+    public Mapper()
+    {
+        var attributedProperties = typeof(TRequest).GetProperties().Where(p => p.PropertyType.IsPrimitive).Where(p => p.GetCustomAttributes(typeof(MapperAttribute), true).Count() > 0);
+    }
+
+}
+
+}
