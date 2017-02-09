34abc6fc5d2efc9975a1d026f6473a45ef40d115
Abhilash Vinod <abvin@outlook.com>
2017-02-08 21:35:35 -0600

    Add property setters

--- ExpressionMapper/Mapper.cs
+++ ExpressionMapper/Mapper.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,12 +24,75 @@ namespace ExpressionMapper
 
     class Mapper<TRequest>
     {
-        Dictionary<string, Func<TRequest, object>> _mappers = new Dictionary<string, Func<TRequest, object>>();
-    public Mapper()
-    {
-        var attributedProperties = typeof(TRequest).GetProperties().Where(p => p.PropertyType.IsPrimitive).Where(p => p.GetCustomAttributes(typeof(MapperAttribute), true).Count() > 0);
-    }
+        class PropertyAccessor
+        {
+            public Func<object, object> Getter { get; set; }
+            public Action<object, object> Setter { get; set; }
+        }
+        private readonly ConcurrentDictionary<Type, ConcurrentDictionary<string, PropertyAccessor>> propertyAccessorCache = new ConcurrentDictionary<Type, ConcurrentDictionary<string, PropertyAccessor>>();
+        public Mapper()
+        {
+            var attributedProperties = typeof(TRequest).GetProperties().Where(p => p.PropertyType.IsPrimitive).Where(p => p.GetCustomAttributes(typeof(MapperAttribute), true).Count() > 0);
+        }
 
-}
+        private void PopulateResponseObject(Hashtable hash, object response)
+        {
+            Type currentType = response.GetType();
+            if (!propertyAccessorCache.ContainsKey(currentType))
+            {
+                throw new Exception("Cannot find mapping for the type");
+            }
+
+            var propertySetters = propertyAccessorCache[currentType];
+            foreach (string hashKey in hash.Keys)
+            {
+                if (!propertySetters.ContainsKey(hashKey))
+                    continue;
+
+                var propertyAccessor = propertySetters[hashKey];
+
+                if (!(hash[hashKey].GetType() == typeof(Hashtable)))
+                {
+                    propertyAccessor.Setter(response, hash[hashKey]);
+                }
+                else //child hashtable
+                {
+                    //instantiate object
+                    propertyAccessor.Setter(response, null);
+                    var childObject = propertyAccessor.Getter(response);
+                    PopulateResponseObject(hash[hashKey] as Hashtable, childObject);
+                }
+            }
+        }
+
+        public Action<object, object> GetPropertySetter(PropertyInfo propertyInfo)
+        {
+            var instance = Expression.Parameter(propertyInfo.DeclaringType, "obj");
+            var valueOfProperty = Expression.Parameter(typeof(object), "valueOfProperty");
+            var typedInstance = Expression.TypeAs(instance, propertyInfo.DeclaringType);
+            if (!propertyInfo.PropertyType.IsClass)
+            {
+                var setterCall = Expression.Call(
+                    typedInstance,
+                    propertyInfo.GetSetMethod(),
+                    Expression.Convert(valueOfProperty, propertyInfo.PropertyType));
+                return (Action<object, object>)Expression.Lambda(setterCall, instance, valueOfProperty)
+                                                .Compile();
+            }
+            else
+            {
+                var constructorExpression = Expression.New(propertyInfo.PropertyType);
+                var setterCall = Expression.Call(
+                    typedInstance,
+                    propertyInfo.GetSetMethod(),
+                    constructorExpression);
+                return (Action<object, object>)Expression.Lambda(setterCall, instance, valueOfProperty)
+                                                .Compile();
+            }
+
+        }
+
+
+    }
 
 }
