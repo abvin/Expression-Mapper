0067c96b0ef38813d2d4e68c5caf9ed9e21f4237
Abhilash Vinod <abvin@outlook.com>
2017-02-08 22:11:38 -0600

    Implement property setters with constructor

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+            ObjectAssert.Equal<ListMapper>(null, null);
+        }
+    }
+
+    public static class ObjectAssert
+    {
+        public static bool Equal<T>(T expected, T actual)
+        {
+            return true;
+        }
+    }
+
+}
